18516eec6469e1a87b586204e94a5ff4d5ef6329
ponske <reuel.euler711@gmail.com>
2024-09-23 14:07:40 +0900

    ドメイン紐づけ

--- wonderpasnavi/wonderpasnavi/settings.py
+++ wonderpasnavi/wonderpasnavi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-2ywi5lhktzhryza&=c&6jj=wf@44(&3j()hkc(ccp^#&w&t2(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['room.bigfanofdisney.com']
 
 
 # Application definition
